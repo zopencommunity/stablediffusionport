2f34a670a681fc3617bc2f003491d5a915869a93
DEVUSER <DEVUSER@S0W1.DAL-EBIS.IHOST.COM>
2025-07-30 23:36:14 +0000

    reverted

--- .zoslib_hooks/zoslib_env_hook.c
+++ .zoslib_hooks/zoslib_env_hook.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #pragma convert("IBM-1047")
-char __zopen_identifier[] = "$Id: Vendor:zOS_Open_Tools BuildRev:2f9e7b8 2025-07-30 22:55:49  $";
+char __zopen_identifier[] = "$Id: Vendor:zOS_Open_Tools BuildRev:04c9b90 2025-07-30 23:29:10  $";
 #pragma convert(pop)
 
 #define PROJECT_ROOT_STR "PROJECT_ROOT"
@@ -22,7 +22,7 @@ long size;
 // by checking if ZOPEN_IN_ZOPEN_BUILD is set to the current build process setting.
 // But this also meant that any dependent tools that set envars via zoslib env hooks would avoid setting those environment variables, effectively breaking them.
 if ((envar_value = getenv("ZOPEN_IN_ZOPEN_BUILD")) &&
-    strcmp(envar_value, "DEVUSER.33620279.11174") == 0) {
+    strcmp(envar_value, "DEVUSER.66055.19121") == 0) {
   return 0;
 }
 
